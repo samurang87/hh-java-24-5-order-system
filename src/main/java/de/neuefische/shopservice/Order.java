94e80e95550c4e09e4fe086266e6a20419ff7f20
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2024-12-03 10:11:04 +0100

    chore: Typo

--- src/main/java/de/neuefische/shopservice/Order.java
+++ src/main/java/de/neuefische/shopservice/Order.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public record Order(int id, List<Product> products) {
 
+
     public Order withoutProduct(Product product){
         List<Product> newProductList = new ArrayList<>(List.copyOf(products));
         newProductList.remove(product);
